1e465e9b29983fe9a7b39fef920b56467d6efeb3
elton-cs <govada.elton@gmail.com>
2024-08-14 16:29:27 -0500

    Add bevy system to display entity count

--- bevy_frontend/src/main.rs
+++ bevy_frontend/src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy_frontend::plugins::{
     display::DisplayPlugin,
+    mock_torii::ToriiPlugin,
     // dojo_to_bevy::DojoToBevy,
     // torii::ToriiPlugin,
 };
@@ -12,6 +13,8 @@ fn main() {
     app.add_systems(Startup, default_camera);
 
     app.add_plugins(DisplayPlugin);
+    app.add_systems(Update, display_entity_count);
+    app.add_plugins(ToriiPlugin);
 
     // app.add_plugins(ToriiPlugin);
     // app.add_plugins(DojoToBevy);
@@ -23,4 +26,12 @@ fn default_camera(mut commands: Commands) {
     let mut camera_bundle = Camera2dBundle::default();
     camera_bundle.projection.scale = 0.3;
     commands.spawn(camera_bundle);
+}
+
+fn display_entity_count(query: Query<Entity>) {
+    let mut total = 0;
+    for _entity in query.iter() {
+        total += 1;
+    }
+    info!("Total entities: {}", total);
 }
