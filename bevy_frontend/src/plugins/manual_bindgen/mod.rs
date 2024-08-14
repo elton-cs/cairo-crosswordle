c620c863904b7c22e4fc1ea6cbce92b85159abb9
elton-cs <govada.elton@gmail.com>
2024-08-14 17:06:40 -0500

    Derive clone for bevy components

--- bevy_frontend/src/plugins/manual_bindgen/mod.rs
+++ bevy_frontend/src/plugins/manual_bindgen/mod.rs
@@ -1,8 +1,6 @@
 use bevy::prelude::Component;
-use cainome::cairo_serde::ContractAddress;
-use starknet::core::types::Felt;
 
-#[derive(Debug, Component)]
+#[derive(Debug, Component, Clone)]
 pub struct Letter {
     pub position: u8,
     pub mock_hash: char,
@@ -10,13 +8,13 @@ pub struct Letter {
     // pub placed_by: ContractAddress,
 }
 
-#[derive(Debug, Component)]
+#[derive(Debug, Component, Clone)]
 pub struct LetterStatus {
     pub position: u8,
     pub status: Status,
 }
 
-#[derive(Debug, Component)]
+#[derive(Debug, Clone)]
 pub enum Status {
     Hidden,
     Solved,
